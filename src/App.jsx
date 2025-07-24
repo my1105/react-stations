2d23237803df0616df70aab05a159ebf1ad2db54
mayu <ykmshiss@gmail.com>
2025-07-25 08:57:49 +0900

    サイトの説明と犬の画像を表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-// DO NOT DELETE
+import { useEffect, useState } from "react";
 
-import './App.css'
+export function App() {
+  const [dogUrl, setDogUrl] = useState("http://localhost:3000/"); 
 
-/**
- * @type {() => JSX.Element}
- */
-export const App = () => {
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setDogUrl(data.message))
+      .catch((err) => console.error("画像取得エラー:", err));
+  }, []);
+
   return (
     <div>
-     <header>Dogアプリ</header>
+      <header>
+        <h1>犬の画像を表示するサイトです</h1>
+      </header>
+      <img src={dogUrl} alt="ランダムな犬" />
     </div>
-  )
+  );
 }
