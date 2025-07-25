9b8e1eca54342fe083dcda72c25a71a05e68b00e
mayu <ykmshiss@gmail.com>
2025-07-25 09:04:39 +0900

    クリック時に stateを更新

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,19 @@ export function App() {
       .catch((err) => console.error("画像取得エラー:", err));
   }, []);
 
+  const handleUpdateClick = () => {
+    console.log("ボタンがクリックされました！");
+    setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg");
+  };
+
   return (
     <div>
       <header>
         <h1>犬の画像を表示するサイトです</h1>
       </header>
       <img src={dogUrl} alt="ランダムな犬" />
+      <br />
+      <button onClick={handleUpdateClick}>更新</button>
     </div>
   );
 }
